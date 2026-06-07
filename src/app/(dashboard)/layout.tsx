d28783e19b86e663aa94f916addf61d4bf881b8e
jumanahjeelani-svg <jumanahjeelani@gmail.com>
2026-06-07 11:33:53 +0300

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -95,7 +95,6 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
               <Menu size={24} />
             </button>
           </div>
-<div className="hidden md:block"><TopBar /></div>
           <main className="flex-1 p-4 md:p-6">{children}</main>
         </div>
       </div>
