8530fe4b36917c47c2a621e392437e9456514747
jumanahjeelani-svg <jumanahjeelani@gmail.com>
2026-06-07 16:18:47 +0300

    Update page.tsx

--- src/app/(dashboard)/scheduler/page.tsx
+++ src/app/(dashboard)/scheduler/page.tsx
@@ -1,10 +1,8 @@
-import TopBar from "@/components/TopBar";
 import SchedulerClient from "@/components/scheduler/SchedulerClient";
 
 export default function SchedulerPage() {
   return (
     <div className="flex flex-col flex-1 overflow-hidden">
-      <TopBar title="جدولة المحتوى" />
       <SchedulerClient />
     </div>
   );
